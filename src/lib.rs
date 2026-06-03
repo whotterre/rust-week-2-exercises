9abf04b60fe13a375fb8d0c0797802283c0429c0
whotterre <ijeddy5.0@gmail.com>
2026-06-03 12:22:55 +0100

    feat: implement logic to decode hex string to native vector and convert byte ordering to big endian

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,14 @@ use hex::{decode, encode};
 
 pub fn decode_hex(hex_str: &str) -> Result<Vec<u8>, String> {
     // TODO: Decode hex string into Vec<u8>, return error string on failure
+     hex::decode(hex_str).map_err(|err| err.to_string())
 }
 
 pub fn to_big_endian(bytes: &[u8]) -> Vec<u8> {
     // TODO: Reverse the byte order of input slice and return as Vec<u8>
+    let reversed: Vec<u8> = bytes.iter().rev().copied().collect();
+    reversed
 }
-
 pub fn bytes_to_hex(bytes: &[u8]) -> String {
     // TODO: Implement conversion of bytes slice to hex string
 }
