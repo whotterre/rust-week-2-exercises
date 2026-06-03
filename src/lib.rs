b13e04d798f5c41db4f1c957e5b30c9fc055ef29
whotterre <ijeddy5.0@gmail.com>
2026-06-03 12:26:28 +0100

    feat: implement logic to convert hex string to byte vector

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,7 @@ pub fn bytes_to_hex(bytes: &[u8]) -> String {
 
 pub fn hex_to_bytes(hex: &str) -> Result<Vec<u8>, hex::FromHexError> {
     // TODO: Implement conversion of hex string to bytes vector
+      hex::decode(hex)
 }
 
 pub fn swap_endian_u32(num: u32) -> [u8; 4] {
