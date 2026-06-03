00ac7249f8068337980debba22fde3a8bc808bd7
whotterre <ijeddy5.0@gmail.com>
2026-06-03 12:24:36 +0100

    feat: implement logic to encode byte array to hex string

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@ pub fn to_big_endian(bytes: &[u8]) -> Vec<u8> {
 }
 pub fn bytes_to_hex(bytes: &[u8]) -> String {
     // TODO: Implement conversion of bytes slice to hex string
+    hex::encode(bytes).to_string()
 }
 
 pub fn hex_to_bytes(hex: &str) -> Result<Vec<u8>, hex::FromHexError> {
